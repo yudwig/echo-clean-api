4d9ac64ac9e545b7106448c5bdf6db82b660498e
yudwig <yudwig.dev@gmail.com>
2021-06-09 20:52:17 +0900

    Add usecases.

--- app/core/usecase/user.go
+++ app/core/usecase/user.go
@@ -2,4 +2,8 @@ package usecase
 
 type UserUseCase interface {
 	CreateUser(name string) error
+	GetUsers() error
+	GetUser(id string) error
+	DeleteUser(id string) error
+	UpdateUserName(id string, name string) error
 }
