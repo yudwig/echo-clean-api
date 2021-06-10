49fbfff8c04033885aa3b2153534bfca6f735456
yudwig <yudwig.dev@gmail.com>
2021-06-10 23:37:50 +0900

    Remove GetUser.

--- app/core/usecase/user.go
+++ app/core/usecase/user.go
@@ -3,7 +3,6 @@ package usecase
 type UserUseCase interface {
 	CreateUser(name string) error
 	GetUsers() error
-	GetUser(id string) error
 	DeleteUser(id string) error
 	UpdateUserName(id string, name string) error
 }
