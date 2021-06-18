ad42475b08f17d9ce3610cc4fc89cd35ede54f2c
yudwig <yudwig.dev@gmail.com>
2021-06-19 01:40:15 +0900

    Implement CLI interfaces.

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-	server "github.com/yudwig/echo-sample/app/driver/echo/server"
+	"fmt"
+	"github.com/yudwig/echo-sample/app/driver/db/rdb"
+	"github.com/yudwig/echo-sample/app/driver/echo/server"
+	"os"
 )
 
+func help() {
+	fmt.Println("Input command.")
+	fmt.Println("- serve: Run server")
+	fmt.Println("- migrate: Run migration")
+}
+
 func main() {
-	server.Run()
+	if len(os.Args) < 2 {
+		help()
+		os.Exit(1)
+	}
+	cmd := os.Args[1]
+	switch cmd {
+	case "serve":
+		server.Run()
+	case "migrate":
+		rdb.Migrate()
+	default:
+		help()
+	}
 }
