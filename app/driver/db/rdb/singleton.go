4f2cb03706ef7c688f86f83f40658facd1202118
yudwig <yudwig.dev@gmail.com>
2021-06-19 01:42:29 +0900

    Add logger to gorm config.

--- app/driver/db/rdb/singleton.go
+++ app/driver/db/rdb/singleton.go
@@ -3,6 +3,10 @@ package rdb
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"time"
 )
 
 type SingletonDbInstance struct {
@@ -13,8 +17,36 @@ type SingletonDbInstance struct {
 var singletonDbInstance = newSingletonDb()
 
 func newSingletonDb() SingletonDbInstance {
+
+	l := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  true,        // Disable color
+		},
+	)
 	dsn := "root@tcp(127.0.0.1:3306)/echo?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		SkipDefaultTransaction:                   false,
+		NamingStrategy:                           nil,
+		FullSaveAssociations:                     false,
+		Logger:                                   l,
+		NowFunc:                                  nil,
+		DryRun:                                   false,
+		PrepareStmt:                              false,
+		DisableAutomaticPing:                     false,
+		DisableForeignKeyConstraintWhenMigrating: false,
+		DisableNestedTransaction:                 false,
+		AllowGlobalUpdate:                        false,
+		QueryFields:                              false,
+		CreateBatchSize:                          0,
+		ClauseBuilders:                           nil,
+		ConnPool:                                 nil,
+		Dialector:                                nil,
+		Plugins:                                  nil,
+	})
 	return SingletonDbInstance{
 		db:  db,
 		err: err,
