8d3871258d5f3d129945c45427e4ca727f41ad4d
yudwig <yudwig.dev@gmail.com>
2021-06-19 01:41:52 +0900

    Add error handling.

--- app/driver/db/rdb/migration.go
+++ app/driver/db/rdb/migration.go
@@ -1,9 +1,15 @@
 package rdb
 
+import "fmt"
+
 func Migrate() {
+	fmt.Println("Migrate start.")
 	db, err := GetDbInstance()
 	if err != nil {
 		panic(err.Error())
 	}
-	db.AutoMigrate(UserModel{})
+	err = db.AutoMigrate(User{})
+	if err != nil {
+		panic(err.Error())
+	}
 }
