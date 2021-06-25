f8dba5e0b510decfe26ea2906ad10d7c3f55f13a
yudwig <yudwig.dev@gmail.com>
2021-06-25 18:15:04 +0900

    Implement mysql user repository.

--- app/driver/db/rdb/users.go
+++ app/driver/db/rdb/users.go
@@ -2,12 +2,12 @@ package rdb
 
 import (
 	"github.com/yudwig/echo-sample/app/core/domain/entity/user"
-	"github.com/yudwig/echo-sample/app/driver/db/seed"
 	"gorm.io/gorm"
+	"strconv"
 )
 
 type UsersRepository struct {
-	db *gorm.DB
+	*Rdb
 }
 
 type User struct {
@@ -16,25 +16,64 @@ type User struct {
 	TimeFields
 }
 
-func NewUsersRepository() (UsersRepository, error) {
-	db, err := GetDbInstance()
-	return UsersRepository{db: db}, err
+func NewUsersRepository(db *gorm.DB) UsersRepository {
+	return UsersRepository{
+		Rdb: &Rdb{db: db},
+	}
 }
 
 func (u UsersRepository) GetAll() ([]user.User, error) {
-	return seed.GetTestUsers()
+	var users []User
+	res := u.db.Find(&users)
+	if res.Error != nil {
+		return []user.User{}, res.Error
+	}
+	var entities []user.User
+	for _, usr := range users {
+		entity, err := user.NewUser(strconv.Itoa(int(usr.Id)), usr.Name)
+		if err != nil {
+			return []user.User{}, err
+		}
+		entities = append(entities, entity)
+	}
+	return entities, nil
 }
 
 func (u UsersRepository) Create(name string) (user.User, error) {
-	usr, _ := user.NewUser("id-1", name)
-	return usr, nil
+	usr := User{
+		Name: name,
+	}
+	res := u.db.Create(&usr)
+	if res.Error != nil {
+		return user.User{}, res.Error
+	}
+	return user.NewUser(strconv.Itoa(int(usr.Id)), usr.Name)
 }
 
 func (u UsersRepository) Delete(id string) error {
+	i, err := strconv.ParseUint(id, 10, 32)
+	if err != nil {
+		return err
+	}
+	usr := User{
+		IdField: IdField{
+			Id: uint(i),
+		},
+	}
+	u.db.Delete(&usr)
 	return nil
 }
 
 func (u UsersRepository) Update(id string, name string) (user.User, error) {
-	usr, _ := user.NewUser(id, name)
-	return usr, nil
+	i, err := strconv.ParseUint(id, 10, 32)
+	if err != nil {
+		return user.User{}, err
+	}
+	usr := User{
+		IdField: IdField{
+			Id: uint(i),
+		},
+	}
+	u.db.Model(&usr).Update("name", name)
+	return user.NewUser(id, name)
 }
