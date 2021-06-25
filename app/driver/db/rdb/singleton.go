d9ea4270d8b3e7b2a3bd77f982f7771a24c56e96
yudwig <yudwig.dev@gmail.com>
2021-06-25 18:14:23 +0900

    Move detail implementation to another file.

--- app/driver/db/rdb/singleton.go
+++ app/driver/db/rdb/singleton.go
@@ -1,12 +1,8 @@
 package rdb
 
 import (
-	"gorm.io/driver/mysql"
+	"flag"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
-	"log"
-	"os"
-	"time"
 )
 
 type SingletonDbInstance struct {
@@ -18,35 +14,15 @@ var singletonDbInstance = newSingletonDb()
 
 func newSingletonDb() SingletonDbInstance {
 
-	l := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
-		logger.Config{
-			SlowThreshold:             time.Second, // Slow SQL threshold
-			LogLevel:                  logger.Info, // Log level
-			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
-			Colorful:                  true,        // Disable color
-		},
+	var (
+		db  *gorm.DB
+		err error
 	)
-	dsn := "root@tcp(127.0.0.1:3306)/echo?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		SkipDefaultTransaction:                   false,
-		NamingStrategy:                           nil,
-		FullSaveAssociations:                     false,
-		Logger:                                   l,
-		NowFunc:                                  nil,
-		DryRun:                                   false,
-		PrepareStmt:                              false,
-		DisableAutomaticPing:                     false,
-		DisableForeignKeyConstraintWhenMigrating: false,
-		DisableNestedTransaction:                 false,
-		AllowGlobalUpdate:                        false,
-		QueryFields:                              false,
-		CreateBatchSize:                          0,
-		ClauseBuilders:                           nil,
-		ConnPool:                                 nil,
-		Dialector:                                nil,
-		Plugins:                                  nil,
-	})
+	if flag.Lookup("test.v") != nil {
+		db, err = NewTestDb()
+	}
+	db, err = NewDb()
+
 	return SingletonDbInstance{
 		db:  db,
 		err: err,
